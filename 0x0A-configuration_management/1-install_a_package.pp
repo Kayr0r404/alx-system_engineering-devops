a526f364aae54763ebe6f410d537d702e44d8cb5
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-25 10:03:50 +0200

    pack innstall

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/pup
-# install flask vesrion 2.1.0 from pip3
+# install werkzeug version 2.1.1 from pip3
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
 
+# install flask version 2.1.0 from pip3 and require werkzeug version 2.1.1
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug'],
 }
