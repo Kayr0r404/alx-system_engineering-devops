90f22b3756653244de8ac9d83a240b5aed35cad3
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-25 02:11:13 +0200

    py

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-#!/usr/bin/pup
-# install flask
+#!/usr/bin/python3
+# install flask vesrion 2.1.0 from pip3
 
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
