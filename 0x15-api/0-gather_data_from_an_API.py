c50cc2fe2ab44d2f98a237d9bb72fd5511cc83a9
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-03 04:52:17 +0200

    get data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,25 +6,25 @@ returns information about his/her TODO list progress
 from sys import argv
 import requests
 
-employee_id = argv[1]
-r_todo = requests.get(
-    'https://jsonplaceholder.typicode.com/users/1/todos?userId={}'.format(
-        employee_id))
-r_usr = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-r_todo.json()
-count = complete = 0
-complted_titles = []
-for dict in r_todo.json():
-    if 'completed' in dict:
-        count += 1
-        if dict['completed'] is True:
-            complted_titles.append(dict['title'])
-            complete += 1
-employee_name = r_usr.json()['name']
-
 
 if __name__ == '__main__':
+    employee_id = argv[1]
+    r_todo = requests.get(
+        'https://jsonplaceholder.typicode.com/users/1/todos?userId={}'.format(
+            employee_id))
+    r_usr = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    r_todo.json()
+    count = complete = 0
+    complted_titles = []
+    for dict in r_todo.json():
+        if 'completed' in dict:
+            count += 1
+            if dict['completed'] is True:
+                complted_titles.append(dict['title'])
+                complete += 1
+    employee_name = r_usr.json()['name']
+
     print('Employee {} is done with tasks({}/{}):'.format(
         employee_name, complete, count))
     for title in complted_titles:
