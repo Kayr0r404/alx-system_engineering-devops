b9d6c6d72e5571552d561fc48ffccd7c1a5ec274
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-18 08:14:33 +0200

    if status code is 404 retun 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''get number of subcribers subcribed to the provided subreddit'''
-    try:
-        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        response = requests.get(url, headers=headers)
-        if response.status_code == 400:
-            return 0
-        else:
-            return response.json()['data']['subscribers']
-    except Exception as e:
-        pass
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'user-agent': 'my-app/0.0.1'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 404:
         return 0
+    else:
+        return response.json()['data']['subscribers']
