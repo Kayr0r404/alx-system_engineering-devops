37ba8f6a6b388d52717687d7e5d2a91b5f602f12
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-18 08:31:31 +0200

    r

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     '''get number of subcribers subcribed to the provided subreddit'''
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'user-agent': 'my-app/0.0.1'}
+    headers = {'user-agent': 'linux:0x16.api.advanced:v1.0.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
