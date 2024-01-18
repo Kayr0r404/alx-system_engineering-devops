5a0ff54abf35868749dbc8009bf36b16b0d4bcfa
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-18 08:11:11 +0200

    user agent added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ def number_of_subscribers(subreddit):
     '''get number of subcribers subcribed to the provided subreddit'''
     try:
         url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()['data']['subscribers']
-        else:
+        headers = {'user-agent': 'my-app/0.0.1'}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 400:
             return 0
+        else:
+            return response.json()['data']['subscribers']
     except Exception as e:
         pass
         return 0
